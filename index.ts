e76f894ae24ca0ca954957d1fab0cfb49876e9b6
Gharis Khalil <gharissheikh678@gmail.com>
2024-04-25 11:40:25 +0500

    my first commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message:"What Is Your Name",type:"input", name:"q1"},
+    { message: "Enter Your First Number", type: "number", name: "firstnumber" },
+    { message: "Enter Your Second Number", type: "number", name: "secondnumber" },
+    {
+      message: "Select one of the operator to perform action",
+      type: "list",
+      name: "operator",
+      choices: ["Addition","Subtraction","Multiplication","Division"],
+    }
+  
+  ]);
+  
+  //Conditional statement
+  
+  if(answer.operator === "Addition"){
+      console.log(answer.firstnumber + answer.secondnumber);
+  }else if(answer.operator === "Subtraction"){
+      console.log(answer.firstnumber - answer.secondnumber);
+  }else if(answer.operator === "Multiplication"){
+      console.log(answer.firstnumber * answer.secondnumber);
+  }else if(answer.operator === "Division"){
+      console.log(answer.firstnumber / answer.secondnumber);
+  }else{
+      console.log("Please select valid operator");
+  }
+  console.log(`${answer.q1}, THANKS FOR USE THIS CALCULATOR`);
